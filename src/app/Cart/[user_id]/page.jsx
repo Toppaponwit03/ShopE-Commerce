f4e5bb59115ccac8721fd7d654bb0df379337033
Toppaponwit03 <toppaponwit1996@hotmail.com>
2024-07-02 22:16:45 +0700

    update

--- src/app/Cart/[user_id]/page.jsx
+++ src/app/Cart/[user_id]/page.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 import axios from 'axios'
 import React,{ useState , useEffect } from 'react'
 
-function Cart({params}) {
+const Cart = ({params}) => {
 
   const [dataCarts , setdataCarts] = useState()
   const [dataProduct , setdataProduct] = useState()
