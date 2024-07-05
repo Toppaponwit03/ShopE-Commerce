addd50e25059486de9284e3f652b4898ec1202fa
Toppaponwit03 <toppaponwit1996@hotmail.com>
2024-07-05 22:50:13 +0700

    updates

--- src/app/components/formProduct.jsx
+++ src/app/components/formProduct.jsx
@@ -1,8 +1,8 @@
 'use client'
-import React from 'react'
+import React,{ useState } from 'react'
 import axios from 'axios'
 import Image from 'next/image';
-import { useState ,useEffect } from 'react';
+
 
 
 
