1c3caef53fc7263f8c0ef4b14ac3e771efeb9030
Toppaponwit03 <toppaponwit1996@hotmail.com>
2024-07-07 12:45:28 +0700

    add Tag GA

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -13,6 +13,21 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+      <script async src={`https://www.googletagmanager.com/gtag/js?id=G-GYHYCE4RG6`}></script>
+      <script 
+            dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+
+              gtag('config', 'G-GYHYCE4RG6');
+            `
+          }}
+      />
+      </head>
+
     <Script
      id="inline-script"
      src="https://kit.fontawesome.com/4a73777019.js" crossorigin="anonymous"
